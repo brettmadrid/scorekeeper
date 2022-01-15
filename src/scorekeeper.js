668aa573a8900da5ed31c25e10148d1f42ba77d0
brettmadrid <brettmadrid9@gmail.com>
2022-01-14 18:55:39 -0800

    updated setState method to use callback form

--- src/scorekeeper.js
+++ src/scorekeeper.js
@@ -7,11 +7,18 @@ class Scorekeeper extends React.Component {
       score: 0,
     };
   }
+
+  singleKill = () => {
+    // when using state itself in the calculation of new state
+    // use this callback form to ensure state updates properly
+    this.setState(curState => ({ score: curState.score + 1 }));
+  };
+
   render() {
     return (
       <div>
         <h1>Score is: {this.state.score}</h1>
-        <button>Single Kill!</button>
+        <button onClick={this.singleKill}>Single Kill!</button>
       </div>
     );
   }
